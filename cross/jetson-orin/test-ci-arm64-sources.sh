7b6ce1d2ab9c7647ff76984bf958351cc603207a
Matthew Doe <matthewdoe782@gmail.com>
2026-03-18 23:29:52 -0400

    ci: isolate ubuntu apt sources for arm64 sysroot

--- cross/jetson-orin/test-ci-arm64-sources.sh
+++ cross/jetson-orin/test-ci-arm64-sources.sh
@@ -22,13 +22,17 @@ assert_not_contains() {
   fi
 }
 
-assert_contains "$workflow_contents" "sudo install -d -m 0755 /etc/apt/arm64-ports.sources.list.d"
-assert_contains "$workflow_contents" "sudo tee /etc/apt/arm64-ports.sources.list.d/ubuntu-ports.sources"
+assert_contains "$workflow_contents" 'sourceparts="$(mktemp -d)"'
+assert_contains "$workflow_contents" 'cat <<'\''EOF'\'' > "$sourceparts/ubuntu-archive-amd64.sources"'
+assert_contains "$workflow_contents" "URIs: http://azure.archive.ubuntu.com/ubuntu"
+assert_contains "$workflow_contents" "Architectures: amd64"
+assert_contains "$workflow_contents" 'cat <<'\''EOF'\'' > "$sourceparts/ubuntu-ports-arm64.sources"'
+assert_contains "$workflow_contents" "URIs: http://ports.ubuntu.com/ubuntu-ports"
 assert_contains "$workflow_contents" "Architectures: arm64"
-assert_contains "$workflow_contents" "-o Dir::Etc::sourceparts=/etc/apt/arm64-ports.sources.list.d"
+assert_contains "$workflow_contents" '-o Dir::Etc::sourceparts="$sourceparts"'
 
-assert_not_contains "$workflow_contents" "sourceparts=\"$(mktemp -d)\""
 assert_not_contains "$workflow_contents" "for src in /etc/apt/sources.list.d/*.sources; do"
-assert_not_contains "$workflow_contents" "Architectures: amd64"
+assert_not_contains "$workflow_contents" "/etc/apt/arm64-ports.sources.list.d"
+assert_not_contains "$workflow_contents" "packages.microsoft.com"
 
 echo "ci arm64 source configuration looks correct"
